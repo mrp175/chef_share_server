eed4dbb1512f068a9fe8ae3a382b00e9785f58ec
Ptolemy <ptolemypowlesland@gmail.com>
2021-06-18 13:02:08 +0200

    fix: Require dotenv so that environment variables can be accessed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-
+require('dotenv').config();
 const router = require('./router');
 const { PORT } = process.env;
 
