c4f044941c1bc1ffb1749ed627606ff92457aadd
Ptolemy Powlesland <ptolemypowlesland@gmail.com>
2021-06-23 15:31:00 +0200

    test: Add tests for logout (#11)

--- test/logout/logout.test.js
+++ test/logout/logout.test.js
@@ -1,51 +1,49 @@
-// require('dotenv').config();
-// const express = require('express');
-// const mocks = require('./mocks');
-// const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
-// const mongoose = require('mongoose');
-// const User = require('../models/user');
-// const server = require('../index.js');
-// const request = require('supertest');
-// const SECRET_KEY = process.env.SECRET_KEY;
-// console.log(SECRET_KEY);
-// server.use(express.json());
-// let {storage} = require('../middlewares/tokenValidation');
-// const token = jwt.sign({_id: 1234}, SECRET_KEY, {expiresIn: '3h'});
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const server = require('../../index.js');
+const request = require('supertest');
+const SECRET_KEY = process.env.SECRET_KEY;
+console.log(SECRET_KEY);
+const token = jwt.sign({_id: '60cdb63d7701481e3450fc8'}, SECRET_KEY, {expiresIn: '3h'});
+const Token = require('../../models/token');
 
-
-// describe('logout should work correctly - endpoint "/logout"', function () { 
-//   beforeEach(function () {
-//     storage = [token];
-//   });
-//   afterEach(function () {
-//     storage = [];
-//   });
-//   it('should logout the user if they are logged in', async function () {
-//     const response = await request(server)
-//     .get('/logout')
-//     .set('Authorization', `Bearer ${token}`);
-//     expect(response.statusCode).toBe(200);
-//   });
-//   it('should return an error if they are not logged in', async function () {
-//     const response = await request(server)
-//     .get('/logout')
-//     .set('Authorization', 'Bearer ')
-//     expect(response.statusCode).toBe(401);
-//   });
-//   it('should logout the user if they are logged in', async function () {
-//     storage.push(token);
-//     const response = await request(server)
-//     .get('/logout')
-//     .set('Authorization', `Bearer ${token}`);
-//     expect(response.body).toBe('logout successful');
-//   });
-//   it('should rremove the token from local storage', async function () {
-//     const response = await request(server)
-//     .get('/logout')
-//     .set('Authorization', `Bearer ${token}`)
-//     console.log(storage);
-//     expect(storage.length).toBe(0);
-//   });
-
-// });
+describe('logout should work correctly - endpoint "/logout"', function () { 
+  beforeEach(async function () {
+    const newToken = await new Token({token});
+    await newToken.save();
+  });
+  afterEach(async function () {
+    await Token.collection.drop();
+  });
+  it('should respond with 200 status code after successfully logging out', async function () {
+    const response = await request(server)
+    .get('/logout')
+    .set('Authorization', `Bearer: ${token}`);
+    expect(response.statusCode).toBe(200);
+  });
+  it('response text should be "logout successful" after successfully logging out', async function () {
+    const response = await request(server)
+    .get('/logout')
+    .set('Authorization', `Bearer: ${token}`);
+    expect(response.text).toBe('logout successful');
+  });
+  it('should remove the token from the database after successfully logging out', async function () {
+    await request(server)
+    .get('/logout')
+    .set('Authorization', `Bearer ${token}`);
+    const checkToken = await Token.findOne({token});
+    expect(checkToken).toBeFalsy();
+  });
+  it('should respond with a 401 status code if the user is not logged in', async function () {
+    const response = await request(server)
+    .get('/logout')
+    .set('Authorization', `Bearer: null`);
+    expect(response.statusCode).toBe(401);
+  });
+  it('response text should be "You need to be logged in first" if the user is not logged in', async function () {
+    const response = await request(server)
+    .get('/logout')
+    .set('Authorization', `Bearer: null`);
+    expect(response.text).toBe('You need to be logged in first');
+  });
+});
