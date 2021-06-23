5b1440f3557c75ac23ee597ce9b32f77765da907
Ptolemy <ptolemypowlesland@gmail.com>
2021-06-23 20:26:56 +0200

    test: Implement tests for the add recipe feature

---
+++ test/addRecipe/mockRecipe.js
@@ -0,0 +1,39 @@
+const recipe = {
+  name: 'Cherry Almond Buckle',
+  keywords: ['cherry almond buckle'],
+  recipeYield: '',
+  recipeIngredient: [
+    '1 and 1/3 cups (166g) all-purpose flour (spoon &amp; leveled)',
+    '1 teaspoon baking powder',
+    '1/4 teaspoon salt',
+    '1/2 cup (115g) unsalted butter, softened to room temperature',
+    '3/4 cup (150g) granulated sugar',
+    '2 large eggs, at room temperature',
+    '1/2 cup (120ml) full-fat sour cream, at room temperature',
+    '1 teaspoon pure vanilla extract',
+    '3/4 teaspoon almond extract*',
+    '1 and 1/2 cups (335g) halved or quartered fresh cherries*',
+    '1/4 cup (31g) all-purpose flour',
+    '1/4 cup (50g) packed light or dark brown sugar',
+    '1/4 teaspoon ground cinnamon',
+    '2 Tablespoons (30g) unsalted butter, melted',
+    '1/4 cup (22g) sliced almonds*'
+  ],
+  image: 'https://cdn.sallysbakingaddiction.com/wp-content/uploads/2021/06/cherry-buckle-225x225.jpg',
+  recipeInstructions: [
+    'Preheat the oven to 350°F (177°C). Grease a 9-inch deep dish pie dish, a 9-inch springform pan, or an 8-inch square baking pan.',
+    'Whisk the flour, baking powder, and salt together. Set aside.',
+    'In a large bowl using a hand mixer or stand mixer fitted with a paddle or whisk attachment, beat the butter and granulated sugar together on high speed until smooth and creamy, about 2 minutes. Scrape down the sides and up the bottom of the bowl with a rubber spatula as needed. Add the eggs, sour cream, vanilla extract, and almond extract. Beat on medium-high speed until combined. The mixture may look curdled; that’s ok. Scrape down the sides and up the bottom of the bowl as needed. With the mixer on low speed, mix in the dry ingredients until smooth. Do not overmix. The batter will be thick. Carefully fold in the cherries and do not overmix. The batter will likely have streaks of pink; that&#8217;s ok. Spoon and spread into prepared pan. Set aside.',
+    'With a fork, mix the flour, brown sugar, and cinnamon together. Stir in the melted butter until crumbs form. Do not overmix because the mixture will turn into a paste. Keep it crumbly. Sprinkle the crumb mixture and sliced almonds evenly on top of batter.',
+    'Bake for 36-42 minutes. Baking times vary, so keep an eye on yours. The buckle is done when a toothpick inserted in the center comes out clean. If you find the top of the buckle is browning too quickly in the oven, loosely cover it with aluminum foil.',
+    'Remove from the oven and set it on a wire rack. Cool for at least 15 minutes before slicing. Those cherries are pretty hot.',
+    'Cover leftover dessert tightly and store in the refrigerator for up to 5 days. Because of the fresh fruit, this dessert tastes best within the first couple days.'
+  ],
+  author: 'Sally',
+  url: 'https://sallysbakingaddiction.com/cherry-almond-buckle/',
+  id: '7bfd4fb4-2808-4857-9f8c-8cbe08c5585b',
+  notes: [],
+  origin: 'test1'
+}
+
+module.exports = recipe;
